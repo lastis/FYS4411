f60b7494e103b64921019d0a0338470145e4216a
lastis <neitakkikkebanan@gmail.com>
2015-02-26 18:30:36 +0100

    Writes all the current parameter to a file main.ini.

--- Project1/src/cpp/assignmentA/parameters.cpp
+++ Project1/src/cpp/assignmentA/parameters.cpp
@@ -6,15 +6,31 @@ using namespace std;
 
 int nDimensions = 3;
 int charge 	= 2;
+int nParticles = 2;
+double stepLength = 1;
+double h = 0.001;
+double h2 = 1000000;
+double alpha = 0.5*charge;
+double beta = 1;
+int nCycles = 1000000;
 
 int main()
 {
+    // TODO Varify that the values are written "exactly" so they can be read
+    // correctly. 
     ofstream myFile;
     myFile.open("main.ini");
-    myFile << "nDimensions = " << nDimensions <<  endl;
     myFile << "charge = " << charge <<  endl;
-    myFile << "nParticles = " << 2 <<  endl;
+    myFile << "alpha = " << alpha << endl;
+    myFile << "beta = " << beta << endl;
+    myFile << "nDimensions = " << nDimensions <<  endl;
+    myFile << "nParticles = " << nParticles <<  endl;
+    myFile << "stepLength = " << stepLength << endl;
+    myFile << "nCycles = " << nCycles << endl;
+    myFile << "h = " << h << endl;
+    myFile << "h2 = " << h2 << endl;
     myFile.close();
+
     VMCSolver solver = VMCSolver();
     solver.useWaveType1();
     solver.runMonteCarloIntegration();
