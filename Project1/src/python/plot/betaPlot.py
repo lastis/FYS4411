91b238ddab898aa9aee61ff5e49ece689a040339
lastis <neitakkikkebanan@gmail.com>
2015-03-04 21:19:00 +0100

    Added plot file.

--- Project1/src/python/plot/betaPlot.py
+++ Project1/src/python/plot/betaPlot.py
@@ -20,22 +20,21 @@ variancefile.close()
 # print len(energyData)
 # print len(energyData[0])
 energy = np.zeros(len(energyData))
+energySq = np.zeros(len(energyData))
 variance = np.zeros(len(energyData))
 std = np.zeros(len(energyData))
 
 N = len(energyData)
 M = len(energyData[0])
+print M
 
 for i in xrange(N):
     for j in xrange(M):
 	energy[i] += energyData[i][j]
-        variance[i] += varianceData[i][j]
+        energySq[i] += energyData[i][j]*energyData[i][j]
     energy[i] = energy[i]/len(energyData[0])
-    variance[i] = variance[i]/len(energyData[0])
-    if (variance[i] >  1.5):
-        variance[i] = variance[i-1]
-    std[i] = np.sqrt(variance[i])
-
+    energySq[i] = energySq[i]/(len(energyData[0]))
+    std[i] = np.sqrt(energySq[i] - energy[i]*energy[i])
 
 plt.plot(betaData, energy)
 plt.show()
