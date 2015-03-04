f833df1fceaf2395ebd43a92c8213a58f4e1b6aa
lastis <neitakkikkebanan@gmail.com>
2015-03-04 15:51:44 +0100

    Added more plots and plot code.

---
+++ Project1/src/cpp/vmcsolver/berylliumAlpha.cpp
@@ -0,0 +1,88 @@
+#include "VMCSolver.h"
+#include <iostream>
+#include <fstream>
+#include <string>
+
+using namespace std;
+
+int main()
+{
+    VMCSolver solver = VMCSolver();
+    solver.charge = 4;
+    solver.alpha = 0; // To be set
+    solver.beta = 0; // To be set
+    solver.nDimensions = 3;
+    solver.nParticles = 4;
+    solver.stepLength = 1.40;
+    solver.nCycles = 10000;
+    solver.waveFunction = solver.WAVE_FUNCTION_BERYLLIUM_1;
+    solver.h = 0.001;
+    solver.h2 = 1e+06;
+    solver.idum = 1;
+    solver.localEnergyFunction = solver.LOCAL_ENERGY_GENERIC;
+
+    int N = 5;
+
+    Vector alpha = Vector(N);
+    Vector beta = Vector(N);
+    Matrix energy = Matrix(N,N);
+
+    ofstream myFile;
+    string base = "../../../res/";
+    string directory = "berylliumAlphaBeta/";
+    string fName;
+    string adress;
+
+    alpha.linspace(2,5);
+    beta.linspace(0.1,2);
+    double* pAlpha = alpha.getArrayPointer();
+    double* pBeta = beta.getArrayPointer();
+    double** pEnergy = energy.getArrayPointer();
+
+    // Run simulation
+    for (int i = 0; i < N; i++) {
+    	for (int j = 0; j < N; j++) {
+	    solver.alpha = pAlpha[i];
+	    solver.beta = pBeta[j];
+	    solver.runIntegration();
+	    pEnergy[i][j] = solver.getEnergy();
+	    cout << "Acceptance ratio : " << solver.getAcceptanceRatio() 
+		<< endl;
+	    cout << "Alpha : " << pAlpha[i] << endl;
+	    cout << "Beta  : "<< pBeta[i] << endl;
+    	}
+    }
+
+    // Store the data.
+    fName = "alpha.txt";
+    cout << "Creating plot file : " << directory + fName << endl;
+    adress = base + directory + fName;
+    myFile.open("../../../res/alphadata.txt");
+    for (int i = 0; i < N; i++) {
+    	myFile << pAlpha[i] << " ";
+    }
+    myFile.close();
+
+    fName = "beta.txt";
+    adress = base + directory + fName;
+    myFile.open(adress.c_str());
+    cout << "Creating plot file : " << directory + fName << endl;
+    for (int i = 0; i < N; i++) {
+    	myFile << pBeta[i] << " ";
+    }
+    myFile.close();
+
+    fName = "energy.txt";
+    cout << "Creating plot file : " << directory + fName << endl;
+    adress = base + directory + fName;
+    myFile.open(adress.c_str());
+    for (int i = 0; i < N; i++) {
+	for (int j = 0; j < N; j++) {
+	    myFile << pEnergy[i][j] << " ";
+	}
+	myFile << endl;
+    }
+    myFile.close();
+    return 0;
+}
+
--- Project1/src/python/plot/alphaPlot2.py
+++ Project1/src/python/plot/alphaPlot2.py
@@ -7,6 +7,12 @@ alphafile = open('../../../res/heliumWave2Alpha/alpha_energy_variance.txt', 'r')
 data = np.loadtxt(alphafile)
 
 plt.plot(data[0], data[1])
+plt.show()
+plt.plot(data[0], data[2])
+# plt.plot(data[0], data[1] - data[2])
+plt.ylim([0,2])
+
+plt.show()
 
 plt.show()
 
