f1a00e522c0073889b34c6b65da9fefdfd3a88f0
Alexander Fleischer <alexander.fleischer@fys.uio.no>
2015-03-04 23:20:06 +0100

    plots

--- Project1/src/python/plot/heliumWave1Wave2.py
+++ Project1/src/python/plot/heliumWave1Wave2.py
@@ -67,33 +67,48 @@ def blocks2(m):
     return blocksizes
 
 print blocks(6)
-'''
+
 x,y = blocking('../../../res/heliumWave1Wave2/wave1Energies.txt','blockVarwave1Energies.txt', np.sort(blocks(6)))
 
 y = np.sort(y)
 
-plt.plot(x, np.sqrt(y))
-plt.title(r'Blocking Analysis of Ground State Energies as Function of STD with' '\n' 
+fig1 = plt.figure()
+ax1 = fig1.add_subplot(111)
+
+ax1.plot(x, np.sqrt(y))
+
+ax1.set_title(r'Blocking Analysis of Ground State Energies as Function of STD with' '\n' 
     r'Generic Energy Calculation and Trial Wavefunction $\psi_{T1}$')
-plt.xlabel(r'Blocksize', fontsize=14)
-plt.ylabel(r'$\sigma$', fontsize=14)
-plt.grid('on')
+ax1.set_xlabel(r'Blocksize', fontsize=14)
+ax1.set_ylabel(r'$\sigma$', fontsize=14)
+
+ax1.grid('on')
+ax1.spines['right'].set_visible(False)
+ax1.spines['top'].set_visible(False)
 
 plt.savefig('../../../res/heliumWave1Wave2/wave1EnergiesBlocking.eps')
 plt.show()
 plt.figure()
-'''
+
+
 x,y = blocking('../../../res/heliumWave1Wave2/wave2Energies.txt','blockVarwave2Energies.txt', np.sort(blocks(6)))
 
 y = np.sort(y)
 
-plt.plot(x, np.sqrt(y))
-plt.title(r'Blocking Analysis of Ground State Energies as Function of STD with' '\n' 
-    r'Generic Energy Calculation and Trial Wavefunction $\psi_{T2}$')
-plt.xlabel(r'Blocksize', fontsize=14)
-plt.ylabel(r'$\sigma$', fontsize=14)
-plt.grid('on')
+fig2 = plt.figure()
+ax2 = fig2.add_subplot(111)
 
-plt.savefig('../../../res/heliumWave1Wave2/wave2EnergiesBlockingScatter.eps')
+ax2.plot(x, np.sqrt(y))
+
+ax2.set_title(r'Blocking Analysis of Ground State Energies as Function of STD with' '\n' 
+    r'Generic Energy Calculation and Trial Wavefunction $\psi_{T2}$')
+ax2.set_xlabel(r'Blocksize', fontsize=14)
+ax2.set_ylabel(r'$\sigma$', fontsize=14)
+
+ax2.grid('on')
+ax2.spines['right'].set_visible(False)
+ax2.spines['top'].set_visible(False)
+
+plt.savefig('../../../res/heliumWave1Wave2/wave2EnergiesBlocking.eps')
 plt.show()
 plt.figure()
